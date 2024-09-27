310072e737f75471471d742160b4e618c273016c
Quang Pham_ MSI <quangpham1019@gmail.com>
2024-09-27 16:14:53 -0700

    redesign the progressBar to include page section buttons that scroll to the respective section on click. the page section buttons are positioned on the progress bar with the same ratio as the section with respect to their position on document.

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,22 +1,53 @@
 $(document).ready(function() {
-    const win = $(window);
-    const doc = $(document);
-    const progressBar = $('progress');
-    const progressLabel = $('.progress-label');
-    const setValue = () => win.scrollTop();
-    const setMax = () => doc.height() - win.height();
-    const setPercent = () => Math.round(win.scrollTop() / (doc.height() - win.height()) * 100);
+    const jDoc = $(document);
+    const jWin = $(window);
+    const jProgressBar = $("#progress-bar");
+    const getCurrentScrollY = () => jWin.scrollTop();
+    const getMaxScrollY = () => jDoc.height() - jWin.height();
 
-    progressLabel.text(setPercent() + '%');
-    progressBar.attr({ value: setValue(), max: setMax() });
+    initializeProgressBar();
+    initializePageSectionButtonsOnProgressBar();
 
-    doc.on('scroll', () => {
-        progressLabel.text(setPercent() + '%');
-        progressBar.attr({ value: setValue() });
-    });
+    function initializeProgressBar() {
+        jProgressBar.attr({ value: getCurrentScrollY(), max: getMaxScrollY() });
+        addEventListenerForProgressBarOnScrollAndWindowResize();
+    }
+    function addEventListenerForProgressBarOnScrollAndWindowResize() {
+        jDoc.on('scroll', () => {
+            jProgressBar.attr({ value: getCurrentScrollY() });
+        });
+        jWin.on('resize', () => {
+            jProgressBar.attr({ value: getCurrentScrollY(), max: getMaxScrollY() });
+        })
+    }
+    function initializePageSectionButtonsOnProgressBar() {
+        const menu = document.getElementById("menu-wrapper");
+        const about = document.getElementById("about");
+        const welcomePane = document.getElementById("welcome-pane");
+        const operationHours = document.getElementById("operation-hours");
 
-    win.on('resize', () => {
-        progressLabel.text(setPercent() + '%');
-        progressBar.attr({ value: setValue(), max: setMax() });
-    })
+        let pageSectionList = [welcomePane, about, menu, operationHours];
+        let pageSection = document.getElementById("progress-bar-section");
+        let curTree = document.createDocumentFragment();
+
+        pageSectionList.forEach((section, index) => {
+            let curPercent = Math.round((section.getBoundingClientRect().top + window.scrollY) / getMaxScrollY() * 100);
+            let newEl = createAndAssignButtonPositionOnProgressBar(index, curPercent);
+
+            newEl.addEventListener('click', () => {
+                window.scroll(0, curPercent/100* getMaxScrollY());
+            });
+            curTree.appendChild(newEl);
+        });
+        pageSection.appendChild(curTree);
+    }
+    function createAndAssignButtonPositionOnProgressBar(index, curPercent) {
+        let newEl = document.createElement("button");
+
+        newEl.id = `button-${index+1}`;
+        newEl.style.position = "absolute";
+        newEl.style.left = `${curPercent}%`;
+
+        return newEl;
+    }
 });
