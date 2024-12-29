019e27f268507b3df857b49f63815c9dc646b888
Quang Pham_ MSI <quangpham1019@gmail.com>
2024-12-29 12:40:17 -0800

    refactor.

--- js/index.js
+++ js/index.js
@@ -1,26 +1,51 @@
 $(document).ready(function() {
 
+    initializeNavbarItems();
+    initializeLogo();
+});
 
-    let content = document.querySelector('.welcome-pane-content');
-    let navItems = document.querySelectorAll('.nav-items h3');
+function initializeNavbarItems() {
+    let navItems = document.querySelectorAll('.nav-list .nav-item');
+
     navItems.forEach(item => {
-        item.addEventListener('click', function(event) {
+        item.firstElementChild.addEventListener('click', function(event) {
             let curItem = event.currentTarget;
-            let toSwap = curItem.id.slice(0, -'-nav'.length);
-            let toSwapEl = document.querySelector(`#${toSwap}`);
 
-            let clone = toSwapEl.cloneNode(true);
-            clone.style.visibility = "initial";
-            content.replaceChildren(clone);
+            swapContent(curItem);
+            updateNavbarIndicator(curItem);
         })
     });
+}
+function swapContent(curItem) {
+    let content = document.querySelector('.content-wrapper-content');
+    let toSwap = curItem.id.slice(0, -'-nav'.length);
+    let toSwapEl = document.querySelector(`#${toSwap}`);
 
-    let welcomeContent = document.querySelector('#welcome-content');
+    if (toSwapEl != null) {
+        let clone = toSwapEl.cloneNode(true);
+        clone.style.visibility = "initial";
+        content.replaceChildren(clone);
+    } else {
+        console.log("cannot find content to substitute");
+    }
+}
+function updateNavbarIndicator(curItem) {
+    let selected = $(".selected");
+    selected.removeClass("selected");
+    curItem.classList.add("selected");
+}
+
+function initializeLogo() {
+    let content = document.querySelector('.content-wrapper-content');
+    let welcomeContent = document.querySelector('#welcome')
     let logo = document.querySelector('.logo');
     logo.addEventListener('click', function(event) {
         let curClone = welcomeContent.cloneNode(true);
         content.replaceChildren(curClone);
+
+        let selected = $(".selected");
+        selected.removeClass("selected");
     })
 
     logo.click();
-});
+}
